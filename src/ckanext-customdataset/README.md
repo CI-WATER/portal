4d0706b1477d5915f49d48b9f39daf18d8a2e6bc
Pabitra Dash <pkdash_reena@hotmail.com>
2014-06-10 15:53:27 -0600

    Moved the ckanext-customdataset to sub folder src/ckanext-customdataset

---
+++ src/ckanext-customdataset/README.md
@@ -0,0 +1,10 @@
+ckanext-customdataset
+=====================
+
+This CKAN extension allows creation of specific dataset types for the CIWATER system.
+Currently it supports the following dataset types:
+
+Model Package (zip file),
+Multidimensional Space Time Dataset (NetCDF),
+Geographic Feature Set (Shapefile), and
+Geographic Raster Dataset (GeoTiff)
---
+++ src/ckanext-customdataset/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-customdataset',
+	version=version,
+	description="This extension allows creation of different types of datasets with each type having unique set of metadata.",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='Pabitra Dash',
+	author_email='pabitra.dash@usu.edu',
+	url='',
+	license='',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.customdataset'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	#ciwater_custom_datasets=ckanext.customdataset.plugin:CustomDatasetsPlugin
+	ciwater_dataset_model_configuration=ckanext.customdataset.plugin:ModelConfigurationPlugin
+	ciwater_dataset_model_package=ckanext.customdataset.plugin:ModelPackagePlugin
+    ciwater_dataset_multidimensional=ckanext.customdataset.plugin:MultidimensionalSpaceTimePlugin
+    ciwater_dataset_geographic_feature_set=ckanext.customdataset.plugin:GeographicFeatureSetPlugin
+    ciwater_dataset_geographic_raster=ckanext.customdataset.plugin:GeographicRasterPlugin
+    ckan_dataset_type=ckanext.customdataset.plugin:DefaultDatasetPlugin
+	""",
+)
